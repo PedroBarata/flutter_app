613fcf5359bbccb1bc9fc21cab455ae838329aa3
pedrobarata <pedrofelipebarata@gmail.com>
2019-06-02 13:21:14 -0300

    feat(23): Diving Deeper Into the Syntax

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
-main() {
-
-}
+void main() => (runApp(MyApp()));
 
 class MyApp extends StatelessWidget {
-  build(context) {
-    return MaterialApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('MyApp'),
+        ),
+      ),
+    );
   }
 }
