13e904b62dd1188db7eeec856cb06523fa5e4028
pedrobarata <pedrofelipebarata@gmail.com>
2019-06-20 20:50:48 -0300

    feat(27): Creating a Stateful Component

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => (runApp(MyApp()));
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyApp();
+  }
+}
+class _MyApp extends State {
+@override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
