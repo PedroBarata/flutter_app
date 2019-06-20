27ca9795d25671d2c205907bf2fe0b8a2b3b870f
pedrobarata <pedrofelipebarata@gmail.com>
2019-06-20 20:15:24 -0300

    feat(24): Adding Cards & Images

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,10 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('MyApp'),
         ),
+        body: Card(child: Column(children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text('Food Paradise')
+        ],),),
       ),
     );
   }
